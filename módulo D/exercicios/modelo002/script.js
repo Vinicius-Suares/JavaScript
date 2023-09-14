12733771c98ed798bfeaee94c257b99a21cba403
Vinícius Suares <142833434+Vinicius-Suares@users.noreply.github.com>
2023-09-14 14:25:55 -0300

    exercicio feito

--- módulo D/exercicios/modelo002/script.js
+++ módulo D/exercicios/modelo002/script.js
@@ -1,11 +1,50 @@
-
 function verificar() {
-    var idade = window.document.getElementById('idade')
-    var i = Number(idade.value)
     var data = new Date()
     var ano = data.getFullYear()
-    var res = window.document.getElementById('resultado')
-    if ( idade.ariaValueMax.length == 0 || idade.value > ano){
+    var fano = document.getElementById('txtano') 
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ' '
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 12) {
+                //crianca
+                img.setAttribute('src', 'bebe-h.jpg')
+            } else if ( idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-h.jpg')
+            } else if ( idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto-h.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-h.jpg')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 12) {
+                //crianca
+                img.setAttribute('src', 'bebe-m.jpg')
+            } else if ( idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-m.jpg')
+            } else if ( idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto-m.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-m.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+        img.setAttribute('width', '300px')     
     }
 }
