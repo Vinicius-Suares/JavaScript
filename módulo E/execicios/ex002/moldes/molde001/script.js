f11a7f6465cd810a53a7cdbbcbc711d05d6d2b26
Vinícius Suares <142833434+Vinicius-Suares@users.noreply.github.com>
2023-09-28 15:27:10 -0300

    resultando

---
+++ módulo E/execicios/ex002/moldes/molde001/script.js
@@ -0,0 +1,15 @@
+function calc() {
+    var numero = document.getElementById('txtnum')
+    var multpli = 1
+    if (numero.value.length == 0) {
+        window.alert = '[ERRO] Número inexistente'
+    } else {
+        var lista = ' '
+    while (multpli <= 10) {
+        var n = Number(numero.value)
+        var m = Number(multpli.value)
+        lista.innerHTML += `${n} x ${m} = ${n*m}`
+        m ++
+    }
+    }
+}
