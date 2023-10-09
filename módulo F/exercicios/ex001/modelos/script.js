986e3e4148a36294924a7e34d6281a6bd8b0e2ac
Vinícius Suares <142833434+Vinicius-Suares@users.noreply.github.com>
2023-10-09 13:31:20 -0300

    das

---
+++ módulo F/exercicios/ex001/modelos/script.js
@@ -0,0 +1,16 @@
+let num = document.getElementById('txtnum')
+let res = document.getElementById('res')
+let lista = document.getElementById('lista')
+
+function inLista() {
+    
+}
+
+
+
+
+function adicionar() {
+    if ( !inLista() && isNumero()) {
+
+    }
+}
